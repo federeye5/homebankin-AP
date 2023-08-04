83276579d52f9d2f81e98f37ed58fe3c8a4e29ed
Federico <federeyes2000@gmail.com>
2023-08-04 04:02:33 -0300

    task1

---
+++ src/main/java/com/mindhub/Homebankin/HomebankinApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.Homebankin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindhub/Homebankin/HomebankinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.Homebankin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
